4618070b6631e388d70e3679bcccf58335fa00cb
Isabelle Rosa <ibell.pereira@gmail.com>
2019-10-21 04:46:08 -0300

    Using memory database on dev && import sql data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO planet(name, type, qtd_moons) VALUES
+  ('Mercury', 'terrestrial', 0),
+  ('Venus', 'terrestrial', 0),
+  ('Earth', 'terrestrial', 1),
+  ('Mars', 'terrestrial', 2),
+  ('Jupiter', 'gas giant', 79),
+  ('Saturn', 'gas giant', 82),
+  ('Uranus', 'ice giant', 27),
+  ('Neptune', 'ice giant', 14),
+  ('Pluto', 'dwarf', 5),
+  ('Ceres', 'dwarf', 0),
+  ('Makemake', 'dwarf', 1),
+  ('Haumea', 'dwarf', 2),
+  ('Eris', 'dwarf', 1);
